6812f1347c34c4aadeb35419288a9f5ebbcd03b8
Arnon Bright <arnonbright@gmail.com>
2024-11-10 15:05:09 +0700

    Deleted styles in App.css and modified index.css and added new component for the site

---
+++ src/assets/Leftpanel.jsx
@@ -0,0 +1,35 @@
+const Leftpanel = () => {
+  return (
+    <>
+      <div className="info">
+        <h1>Arnon Bright</h1>
+        <h3>Frontend Developer</h3>
+        <span>Rayong, Thailand</span>
+        <p>
+          I'm a self-taught frontend developer with solid skills in HTML, CSS,
+          JavaScript, and React, I'm motivated to keep learning, solve problems,
+          and make an impact on real projects.
+        </p>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <a href="#">github</a>
+          </li>
+          <li>
+            <a href="#">linkedin</a>
+          </li>
+          <li>
+            <a href="#">Email</a>
+          </li>
+          <li>
+            <a href="#">IG</a>
+          </li>
+        </ul>
+      </div>
+      <span>Developed by Arnon Bright</span>
+    </>
+  );
+};
+
+export default Leftpanel;
---
+++ src/assets/Rightpanel.jsx
@@ -0,0 +1,49 @@
+const Rightpanel = () => {
+  return (
+    <>
+      <div className="projects">
+        <h2>Projects</h2>
+        <p>
+          Below is a selection of projects I’ve developed over my 9+ months of
+          coding. This highlights just a portion of the work I’ve done, as
+          several other projects didn’t make the list. Some are still
+          unfinished—either due to skill limitations at the time or because I
+          pursued more engaging challenges. I plan to revisit and refine these
+          projects in the future, enhancing their code with my current
+          expertise.
+        </p>
+      </div>
+      <div className="project-links">
+        <ul>
+          <li>
+            <a href="#">Project1</a>
+          </li>
+          <li>
+            <a href="#">Project2</a>
+          </li>
+          <li>
+            <a href="#">Project3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="exp">
+        <h2>Experience</h2>
+        <p>
+          Error 404: No industry experience… yet! While I may not have
+          professional experience, I bring over 9 months of hands-on coding and
+          frontend development work through personal projects and self-driven
+          learning.
+        </p>
+        <p>
+          My projects have helped me build strong skills in HTML, CSS,
+          JavaScript, and React, along with a solid foundation in
+          problem-solving, collaboration, and adaptability. I'm excited to apply
+          my skills and passion for technology in a professional setting, where
+          I can continue to grow and develop as a frontend developer.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Rightpanel;
